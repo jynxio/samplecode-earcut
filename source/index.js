48607eebd00cc86382fe9f9aa4bee9af9613978f
X <1337816495@qq.com>
2022-04-20 20:58:22 +0800

    u

--- source/index.js
+++ source/index.js
@@ -1,41 +1,27 @@
 import "/style/reset.css";
 
 import * as three from "three";
-
 import earcut from "earcut";
 
-/* ------------------------------------------------------------------------------------------------------ */
 /* CSS */
+/* ------------------------------------------------------------------------------------------------------ */
 document.body.style.backgroundColor = "#000";
 
+/* Base */
 /* ------------------------------------------------------------------------------------------------------ */
-/* Renderer */
 const renderer = new three.WebGLRenderer( { antialias: window.devicePixelRatio < 2 } );
 
 renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
 renderer.setSize( window.innerWidth, window.innerHeight );
-
 document.body.append( renderer.domElement );
 
-/* Scene */
 const scene = new three.Scene();
-
-/* Camera */
 const aspect_ratio = window.innerWidth / window.innerHeight;
-const camera = new three.OrthographicCamera(
-    - 100 * aspect_ratio, // left
-    + 100 * aspect_ratio, // right
-    + 100,                // top
-    - 100,                // bottom
-    0.1,                  // near
-    100,                  // far
-);
+const camera = new three.OrthographicCamera( - 180 * aspect_ratio, 180 * aspect_ratio, 180, - 180, 0.1, 10 );
 
 camera.position.set( 0, 0, 1 );
-
 scene.add( camera );
 
-/* Resize */
 window.addEventListener( "resize", _ => {
 
     renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
@@ -44,63 +30,120 @@ window.addEventListener( "resize", _ => {
     const aspect_ratio = window.innerWidth / window.innerHeight;
 
     camera.left = - 100 * aspect_ratio;
-    camera.right = + 100 * aspect_ratio;
+    camera.right = 100 * aspect_ratio;
     camera.updateProjectionMatrix();
 
 } );
 
-/* Render */
 renderer.setAnimationLoop( function loop() {
 
     renderer.render( scene, camera );
 
 } );
 
+/* Simple Code */
 /* ------------------------------------------------------------------------------------------------------ */
-/*  */
-const geometry = new three.BufferGeometry();
-const material_background = new three.MeshBasicMaterial( { color: 0x333333 } );
-const material_bone = new three.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+const polygon_without_hole = new PolygonWithoutHole();
+const polygon_with_a_hole = new PolygonWithAHole();
+const polygon_with_four_hole = new PolygonWithFourHole();
 
-const mesh_background = new three.Mesh( geometry, material_background );
-const mesh_bone = new three.Mesh( geometry, material_bone );
+polygon_without_hole.translateX( - 120 );
+polygon_with_four_hole.translateX( 120 );
 
-scene.add( mesh_background, mesh_bone );
+scene.add( polygon_without_hole );
+scene.add( polygon_with_a_hole );
+scene.add( polygon_with_four_hole );
 
+function PolygonWithoutHole() {
 
-/**
- * GeoJSON规定，boundary的
- */
-const data = [
-    - 50, + 50, 0, // boundary 左上角
-    - 50, - 50, 0, // boundary 左下角
-    + 50, - 50, 0, // boundary 右下角
-    + 50, + 50, 0, // boundary 右上角
-    - 40, + 40, 0, // 孔1 左上角（三角形）
-    + 30, + 40, 0, // 孔1 右上角（三角形）
-    - 40, - 30, 0, // 孔1 左下角（三角形）
-    + 40, - 40, 0, // 孔2 右下角（三角形）
-    - 30, - 40, 0, // 孔2 左下角（三角形）
-    + 40, + 30, 0, // 孔2 右上角（三角形）
-];
+    const vertices = [
+        - 50, + 50, 0, // 轮廓：左上
+        - 50, - 50, 0, // 轮廓：左下
+        + 50, - 50, 0, // 轮廓：右下
+        + 50, + 50, 0, // 轮廓：右上
+    ];
+    const triangulation = earcut( vertices, null, 3 );
+    const polygon = new Polygon( vertices, triangulation );
 
-let position;
+    return polygon;
 
-position = new Float32Array( data );
-position = new three.BufferAttribute( position, 3 );
+}
 
-geometry.setAttribute( "position", position );
+function PolygonWithAHole() {
 
-/*  */
-let index;
+    const vertices = [
+        - 50, + 50, 0, // 轮廓：左上
+        - 50, - 50, 0, // 轮廓：左下
+        + 50, - 50, 0, // 轮廓：右下
+        + 50, + 50, 0, // 轮廓：右上
 
-index = earcut( data, [ 4, 7 ], 3 ); // 0~3号顶点是图形的轮廓，4~6号顶点是孔1的轮廓，7~9号顶点是孔2的轮廓
-index = new Uint16Array( index );
-index = new three.BufferAttribute( index, 1 );
+        - 30, + 30, 0, // 孔：左上
+        + 30, + 30, 0, // 孔：右上
+        + 30, - 30, 0, // 孔：右下
+        - 30, - 30, 0, // 孔：左下
+    ];
+    const triangulation = earcut( vertices, [ 4 ], 3 );
+    const polygon = new Polygon( vertices, triangulation );
 
-geometry.setIndex( index );
-geometry.index.needsUpdate = true;
+    return polygon;
 
-/*  */
-geometry.computeBoundingBox();
-geometry.computeBoundingSphere();
+}
+
+function PolygonWithFourHole() {
+
+    const vertices = [
+        - 50, + 50, 0, // 轮廓：左上
+        - 50, - 50, 0, // 轮廓：左下
+        + 50, - 50, 0, // 轮廓：右下
+        + 50, + 50, 0, // 轮廓：右上
+
+        - 15, + 15, 0, // 孔：左上
+        -  5, + 15, 0, // 孔：右上
+        -  5, +  5, 0, // 孔：右下
+        - 15, +  5, 0, // 孔：左下
+
+        +  5, + 15, 0, // 孔：左上
+        + 15, + 15, 0, // 孔：右上
+        + 15, +  5, 0, // 孔：右下
+        +  5, +  5, 0, // 孔：左下
+
+        +  5, -  5, 0, // 孔：左上
+        + 15, -  5, 0, // 孔：右上
+        + 15, - 15, 0, // 孔：右下
+        +  5, - 15, 0, // 孔：左下
+
+        - 15, -  5, 0, // 孔：左上
+        -  5, -  5, 0, // 孔：右上
+        -  5, - 15, 0, // 孔：右下
+        - 15, - 15, 0, // 孔：左下
+    ];
+    const triangulation = earcut( vertices, [ 4, 8, 12, 16 ], 3 );
+    const polygon = new Polygon( vertices, triangulation );
+
+    return polygon;
+
+}
+
+function Polygon( position, triangulation ) {
+
+    const index_attribute = new three.BufferAttribute( new Uint16Array( triangulation ), 1 );
+    const position_attribute = new three.BufferAttribute( new Float32Array( position ), 3 );
+    const geometry = new three.BufferGeometry();
+
+    geometry.setIndex( index_attribute );
+    geometry.index.needsUpdate = true;
+    geometry.setAttribute( "position", position_attribute );
+    geometry.computeBoundingBox();
+    geometry.computeBoundingSphere();
+
+    const material_background = new three.MeshBasicMaterial( { color: 0x333333 } );
+    const material_bone = new three.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    const mesh_background = new three.Mesh( geometry, material_background );
+    const mesh_bone = new three.Mesh( geometry, material_bone );
+    const group = new three.Group();
+
+    group.add( mesh_background, mesh_bone );
+
+    return group;
+
+}
